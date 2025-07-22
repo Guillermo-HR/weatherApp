28df71b91f544e2baba848cd91876f48ad8317e7
Guillermo <guillermo.hre@gmail.com>
2025-07-21 22:46:06 -0600

    Add initial implementation of weather and air quality data extraction

---
+++ etl/pipeline.py
@@ -0,0 +1,77 @@
+"""
+Pipeline for extracting, transforming, and loading weather and air quality data.
+Limits for Mexico City are:
+* North: 19.60
+* South: 19.05
+* East: -98.95
+* West: -99.35
+Grid size is 0.05 degrees.
+"""
+
+from datetime import datetime, timezone 
+
+from extract import Extract
+from config.secrets import get_secrets
+from config.arguments import get_args
+
+def get_extractors(*api_data)-> dict:
+    extractors = {}
+    for api in api_data:
+        api_name = api["api_name"]
+        extractors[api_name] = Extract(
+            api_name=api_name,
+            api_key=api["api_key"],
+            constant_params=api["constant_params"],
+            search_params=api["search_params"],
+            api_base_url=api["api_base_url"]
+        )
+    return extractors
+
+def extract(data_coordinates:dict, extractors: dict)-> dict:
+    data = {}
+    for name, extractor in extractors.items():
+        data[name] = []
+
+    for lat in data_coordinates["lat"]:
+        for lon in data_coordinates["lon"]:
+            dt = datetime.now(timezone.utc).timestamp()
+            for name, extractor in extractors.items():
+                response = extractor.get_data(lat, lon)
+                if response["status"] == "failed":
+                    continue
+                data[name].append({
+                    "lat": lat,
+                    "lon": lon,
+                    "data": response["data"],
+                    "timestamp": dt
+                })
+
+    return data
+
+def main():
+    app_secrets = get_secrets()
+    app_args = get_args()
+    api_data = [{
+        "api_name": "Open weather weather",
+        "api_key": f'&appid={app_secrets["OPEN_WEATHER_API_KEY"]}',
+        "constant_params": "&units=metric&lang=es",
+        "search_params": "lat={lat}&lon={lon}",
+        "api_base_url": "https://api.openweathermap.org/data/2.5/weather?",
+    },
+    {
+        "api_name": "Open weather air quality",
+        "api_key": f'&appid={app_secrets["OPEN_WEATHER_API_KEY"]}',
+        "constant_params": "&lang=es",
+        "search_params": "lat={lat}&lon={lon}",
+        "api_base_url": "http://api.openweathermap.org/data/2.5/air_pollution?",
+    }]
+    extractors = get_extractors(*api_data)
+    data_coordinates = {
+        "lat": [19.60, 19.05],
+        "lon": [-98.95, -99.35]
+    }
+
+    data = extract(data_coordinates, extractors)
+
+if __name__ == "__main__":
+    main()
