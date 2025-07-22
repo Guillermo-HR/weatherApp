25f4fc406f6b9b493ffbba402c01072a0f434c7f
Guillermo <guillermo.hre@gmail.com>
2025-07-21 23:18:10 -0600

    Refactor argument parsing to correct latitude and longitude defaults and rename grid argument to grid_size

--- etl/config/arguments.py
+++ etl/config/arguments.py
@@ -2,9 +2,10 @@ import argparse
 
 def get_args()->argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--max_lon", type=float, default=19.60)
-    parser.add_argument("--min_lon", type=float, default=19.05)
-    parser.add_argument("--max_lat", type=float, default=-98.95)
-    parser.add_argument("--min_lat", type=float, default=-99.35)
-    parser.add_argument("--grid", type=float, default=0.05)
+    parser.add_argument("--max_lat", type=float, default=19.55)
+    parser.add_argument("--min_lat", type=float, default=19.40)
+    parser.add_argument("--max_lon", type=float, default=-99.05)
+    parser.add_argument("--min_lon", type=float, default=-99.20)
+    parser.add_argument("--grid_size", type=float, default=0.05)
+    
     return parser.parse_args()
