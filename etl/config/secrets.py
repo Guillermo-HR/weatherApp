96d07b2db8346c8fa92f651258366f03bc0fc155
Guillermo <guillermo.hre@gmail.com>
2025-07-28 18:58:06 -0600

    Add database configuration secrets to the get_secrets function

--- etl/config/secrets.py
+++ etl/config/secrets.py
@@ -8,5 +8,10 @@ def get_secrets()->Dict:
 
     OPEN_WEATHER_API_KEY = os.getenv("OPEN_WEATHER_API_KEY")
     return {
-        "OPEN_WEATHER_API_KEY": OPEN_WEATHER_API_KEY
+        "OPEN_WEATHER_API_KEY": OPEN_WEATHER_API_KEY,
+        "DB_USER": os.getenv("DB_USER"),
+        "DB_PASSWORD": os.getenv("DB_PASSWORD"),
+        "DB_HOST": os.getenv("DB_HOST"),
+        "DB_NAME": os.getenv("DB_NAME"),
+        "DB_PORT": int(os.getenv("DB_PORT", 5432))
     }
