dd28854204dc7ec4ce8254109495ddcfcb923303
Guillermo <guillermo.hre@gmail.com>
2025-08-01 13:09:56 -0600

    Update argument defaults and modify database secret keys in configuration

--- etl/config/arguments.py
+++ etl/config/arguments.py
@@ -3,9 +3,9 @@ import argparse
 def get_args()->argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("--max_lat", type=float, default=19.55)
-    parser.add_argument("--min_lat", type=float, default=19.40)
+    parser.add_argument("--min_lat", type=float, default=19.50)
     parser.add_argument("--max_lon", type=float, default=-99.05)
-    parser.add_argument("--min_lon", type=float, default=-99.20)
+    parser.add_argument("--min_lon", type=float, default=-99.10)
     parser.add_argument("--grid_size", type=float, default=0.05)
     
     return parser.parse_args()
--- etl/config/secrets.py
+++ etl/config/secrets.py
@@ -9,9 +9,9 @@ def get_secrets()->Dict:
     OPEN_WEATHER_API_KEY = os.getenv("OPEN_WEATHER_API_KEY")
     return {
         "OPEN_WEATHER_API_KEY": OPEN_WEATHER_API_KEY,
-        "DB_USER": os.getenv("DB_USER"),
-        "DB_PASSWORD": os.getenv("DB_PASSWORD"),
-        "DB_HOST": os.getenv("DB_HOST"),
-        "DB_NAME": os.getenv("DB_NAME"),
-        "DB_PORT": int(os.getenv("DB_PORT", 5432))
+        "DB_USER": os.getenv("DATABASE_USER"),
+        "DB_PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "DB_HOST": os.getenv("DATABASE_HOST"),
+        "DB_NAME": os.getenv("DATABASE_NAME"),
+        "DB_PORT": int(os.getenv("DATABASE_PORT", 5432))
     }
