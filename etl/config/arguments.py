099aa17f83738f1738d7138962a63ab76a0dcd5e
Guillermo <guillermo.hre@gmail.com>
2025-08-01 13:51:07 -0600

    Rename latitude and longitude arguments for consistency in argument parsing

--- etl/config/arguments.py
+++ etl/config/arguments.py
@@ -2,10 +2,10 @@ import argparse
 
 def get_args()->argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--max_lat", type=float, default=19.55)
-    parser.add_argument("--min_lat", type=float, default=19.50)
-    parser.add_argument("--max_lon", type=float, default=-99.05)
-    parser.add_argument("--min_lon", type=float, default=-99.10)
+    parser.add_argument("--max_latitude", type=float, default=19.55)
+    parser.add_argument("--min_latitude", type=float, default=19.50)
+    parser.add_argument("--max_longitude", type=float, default=-99.05)
+    parser.add_argument("--min_longitude", type=float, default=-99.10)
     parser.add_argument("--grid_size", type=float, default=0.05)
     
     return parser.parse_args()
