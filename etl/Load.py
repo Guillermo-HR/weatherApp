e9c7ce216341cfb527a4e420416dbd6f37dba564
Guillermo <guillermo.hre@gmail.com>
2025-08-01 16:04:40 -0600

    Implement Load class for data loading functionality with error handling

---
+++ etl/Load.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import logging
+
+class Load:
+    def __init__(self, engine):
+        self.engine = engine
+        self.logger = logging.getLogger(f"load_extractor")
+
+    def load_data(self, data:dict, table:str) -> tuple[int, int]:
+        successful = 0
+        failed = 0
+
+        try:
+            df = pd.DataFrame(data)
+            df['recorded_at'] = pd.to_datetime(df['recorded_at'], unit='s', utc=True)
+            df.to_sql(table, self.engine, if_exists='append', index=False)
+            successful = len(df)
+            failed = 0
+        except Exception as e:
+            logging.error(f"Failed to load data into {table}: {e}")
+            failed = len(data)
+        
+        return successful, failed
