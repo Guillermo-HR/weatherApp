e3b9b64efe9ca227892fc8ba83366bfa5888a841
Guillermo <guillermo.hre@gmail.com>
2025-08-04 12:41:00 -0600

    Pass arguments to the ETL pipeline script in cron job

--- etl/cronjob_weatherapp_etl.sh
+++ etl/cronjob_weatherapp_etl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/ghre/Desktop/weatherApp/.venv/bin/activate
-python -u "/home/ghre/Desktop/weatherApp/etl/pipeline.py"
+python -u "/home/ghre/Desktop/weatherApp/etl/pipeline.py" "$@"
 deactivate
