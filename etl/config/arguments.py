7d96c3173ae82e1225bf288e6d41cab177bf71fc
Guillermo <guillermo.hre@gmail.com>
2025-08-04 18:26:39 -0600

    Restore default values for latitude, longitude, and grid size in argument parser

--- etl/config/arguments.py
+++ etl/config/arguments.py
@@ -2,10 +2,10 @@ import argparse
 
 def get_args()->argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--max_latitude", type=float, default=19.55)
-    parser.add_argument("--min_latitude", type=float, default=19.50)
-    parser.add_argument("--max_longitude", type=float, default=-99.05)
-    parser.add_argument("--min_longitude", type=float, default=-99.10)
-    parser.add_argument("--grid_size", type=float, default=0.05)
+    parser.add_argument("--max_latitude", type=float, default=19.37)
+    parser.add_argument("--min_latitude", type=float, default=19.29)
+    parser.add_argument("--max_longitude", type=float, default=-99.13)
+    parser.add_argument("--min_longitude", type=float, default=-99.20)
+    parser.add_argument("--grid_size", type=float, default=0.02)
     
     return parser.parse_args()
