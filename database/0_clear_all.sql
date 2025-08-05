fdbc68bba8ed85493e3a84a638730d266e43be1f
Guillermo <guillermo.hre@gmail.com>
2025-08-04 18:26:23 -0600

    Add termination of active connections before dropping the weatherapp_database

--- database/0_clear_all.sql
+++ database/0_clear_all.sql
@@ -1,3 +1,8 @@
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE pg_stat_activity.datname = 'weatherapp_database'
+  AND pid <> pg_backend_pid();
+
 drop database if exists weatherapp_database;
 
 DO $$
