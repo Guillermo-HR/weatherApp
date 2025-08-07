38f628167e468c64daabe4ac7e9ce979327f6fbe
Guillermo <guillermo.hre@gmail.com>
2025-08-07 15:31:34 -0600

    Remove API table and related data loading script from database configuration

--- database/ddl/4_create_tables.sql
+++ database/ddl/4_create_tables.sql
@@ -8,13 +8,6 @@ create table weatherapp_schema.zone (
     constraint zone_latitude_longitude_grid_size_uq unique (latitude, longitude, grid_size)
 );
 
-create table weatherapp_schema.api (
-    id serial primary key,
-    name text not null,
-
-    constraint api_name_uq unique (name)
-);
-
 create table weatherapp_schema.weather (
     id serial primary key,
     temperature double precision not null,
@@ -22,10 +15,8 @@ create table weatherapp_schema.weather (
     pressure double precision not null,
     recorded_at timestamp with time zone default current_timestamp,
     zone_id integer not null,
-    api_id integer not null,
 
-    constraint weather_zone_id_fkey foreign key (zone_id) references weatherapp_schema.zone(id),
-    constraint weather_api_id_fkey foreign key (api_id) references weatherapp_schema.api(id)
+    constraint weather_zone_id_fkey foreign key (zone_id) references weatherapp_schema.zone(id)
 );
 CREATE INDEX idx_weather_recorded_at ON weatherapp_schema.weather(recorded_at);
 
@@ -41,9 +32,7 @@ create table weatherapp_schema.air_quality (
     nh3 float not null,
     recorded_at timestamp with time zone default current_timestamp,
     zone_id integer not null,
-    api_id integer not null,
 
-    constraint air_quality_zone_id_fkey foreign key (zone_id) references weatherapp_schema.zone(id),
-    constraint air_quality_api_id_fkey foreign key (api_id) references weatherapp_schema.api(id)
+    constraint air_quality_zone_id_fkey foreign key (zone_id) references weatherapp_schema.zone(id)
 );
 CREATE INDEX idx_air_quality_recorded_at ON weatherapp_schema.air_quality(recorded_at);
