5f818ef330aa0dff3e78425e1c25a082511b1fd4
Guillermo <guillermo.hre@gmail.com>
2025-08-08 17:59:35 -0600

    Refactor AirQualityTransformer to remove grid_size dependency; update validation and transformation logic for improved data handling and clarity.

--- etl/transform/AirQualityTransformer.py
+++ etl/transform/AirQualityTransformer.py
@@ -4,14 +4,16 @@ from typing import Dict
 
 class AirQualityTransformer:
     def __init__(self, zone_ids: pd.DataFrame):
+        self.columns = ["co", "no", "no2", "o3", "so2", "pm2_5", "pm10", "nh3", 
+                        "zone_id", "recorded_at"]
         self.zone_map = {
-            (row['latitude'], row['longitude'], row['grid_size']): row['id']
+            (row['latitude'], row['longitude']): row['id']
             for _, row in zone_ids.iterrows()
         }
-        self.metadata_keys = ["latitude", "longitude", "grid_size", "data", "timestamp"]
-        self.data_keys = ["co", "no", "no2", "o3", "so2", "pm2_5", "pm10", "nh3"]
+        self.metadata_keys = ["latitude", "longitude", "timestamp"]
         self.rules = {
-            "recorded_at": {"type": (float, int), "min": 0},
+            "recorded_at": {"type": (int), "min": 0},
+            "zone_id": {"type": (int), "min": 0},
             "co": {"type": (float, int), "min": 0},
             "no": {"type": (float, int), "min": 0},
             "no2": {"type": (float, int), "min": 0},
@@ -23,21 +25,16 @@ class AirQualityTransformer:
         }
         self.logger = logging.getLogger(f"air_quality_transformer")
 
-    def validate_structure(self, raw_data:dict) -> bool:
+    def validate_structure(self, record:dict) -> bool:
         for key in self.metadata_keys:
-            if key not in raw_data:
+            if key not in record:
                 self.logger.error(f"Missing key in raw data: {key}")
                 return False
-            
-        if type(raw_data.get("data")) is not Dict:
+
+        if "data" not in record or type(record["data"]) is not dict:
             self.logger.error("Invalid data structure.")
             return False
 
-        for key in self.data_keys:
-            if key not in raw_data['data']['list'][0]["components"]:
-                self.logger.error(f"Missing key in raw data: {key}")
-                return False
-            
         return True
 
     def validate_data(self, data: Dict) -> bool:
@@ -57,28 +54,38 @@ class AirQualityTransformer:
             
         return True
 
-    def transform(self, raw_data: Dict) -> Dict:
+    def get_zone(self, latitude: float, longitude: float) -> int:
+        if (latitude, longitude) in self.zone_map:
+            zone_id = self.zone_map[(latitude, longitude)]
+            return int(zone_id)
+        else:
+            self.logger.error(f"Zone not found for coordinates: ({latitude}, {longitude})")
+            return -1
+
+    def transform(self, record: Dict) -> Dict:
+        if not self.validate_structure(record):
+            return {}
+
+        latitude = record["latitude"]
+        longitude = record["longitude"]
+        zone_id = self.get_zone(latitude, longitude)
+        if zone_id == -1:
+            return {}
+
         transformed_data = {
-            "recorded_at": raw_data.get("timestamp"),
-            "co": raw_data["data"]["list"][0]["components"].get("co"),
-            "no": raw_data["data"]["list"][0]["components"].get("no"),
-            "no2": raw_data["data"]["list"][0]["components"].get("no2"),
-            "o3": raw_data["data"]["list"][0]["components"].get("o3"),
-            "so2": raw_data["data"]["list"][0]["components"].get("so2"),
-            "pm2_5": raw_data["data"]["list"][0]["components"].get("pm2_5"),
-            "pm10": raw_data["data"]["list"][0]["components"].get("pm10"),
-            "nh3": raw_data["data"]["list"][0]["components"].get("nh3"),
+            "recorded_at": int(record["timestamp"]),
+            "zone_id": zone_id,
+            "co": record["data"]["OPEN_WEATHER_AIR_QUALITY"]["list"][0]["components"].get("co"),
+            "no": record["data"]["OPEN_WEATHER_AIR_QUALITY"]["list"][0]["components"].get("no"),
+            "no2": record["data"]["OPEN_WEATHER_AIR_QUALITY"]["list"][0]["components"].get("no2"),
+            "o3": record["data"]["OPEN_WEATHER_AIR_QUALITY"]["list"][0]["components"].get("o3"),
+            "so2": record["data"]["OPEN_WEATHER_AIR_QUALITY"]["list"][0]["components"].get("so2"),
+            "pm2_5": record["data"]["OPEN_WEATHER_AIR_QUALITY"]["list"][0]["components"].get("pm2_5"),
+            "pm10": record["data"]["OPEN_WEATHER_AIR_QUALITY"]["list"][0]["components"].get("pm10"),
+            "nh3": record["data"]["OPEN_WEATHER_AIR_QUALITY"]["list"][0]["components"].get("nh3"),
         }
 
         if not self.validate_data(transformed_data):
             return {}
-        
-        latitude = raw_data.get("latitude")
-        longitude = raw_data.get("longitude")
-        grid_size = raw_data.get("grid_size")
-        zone_id = self.zone_map.get((latitude, longitude, grid_size))
-        
-        transformed_data["zone_id"] = zone_id
-        transformed_data["recorded_at"] = int(transformed_data["recorded_at"])
         
         return transformed_data
