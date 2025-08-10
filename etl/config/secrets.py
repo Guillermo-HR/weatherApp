35cd334aaddda9d466ce32226db766d67b985772
Guillermo <guillermo.hre@gmail.com>
2025-08-10 14:03:20 -0600

    Refactor argument and secret retrieval functions to include logging for error handling and success messages

--- etl/config/secrets.py
+++ etl/config/secrets.py
@@ -1,9 +1,10 @@
 from dotenv import load_dotenv
 import os
 from typing import Dict
+import logging
 
 
-def get_secrets(target_table: str) -> Dict:
+def get_secrets(target_table: str, logger: logging.Logger) -> Dict:
     tables = {
         "weather": {
             "OPEN_WEATHER_WEATHER": "OPEN_WEATHER_API_KEY"
@@ -14,20 +15,25 @@ def get_secrets(target_table: str) -> Dict:
     }
 
     if target_table not in tables:
+        logger.critical(f"target_table '{target_table}' is not supported.")
         raise ValueError(f"target_table '{target_table}' is not supported.")
     
     load_dotenv()
 
     required_apis = {key: os.getenv(value) for key, value in tables[target_table].items()}
     if not all(required_apis.values()):
-        raise ValueError("One or more API keys are missing in the environment variables.")
+        logger.critical("One or more API keys are missing in the environment variables.")
+        return None # type: ignore
 
     database = ["DATABASE_USER", "DATABASE_PASSWORD", "DATABASE_HOST", "DATABASE_NAME", 
                 "DATABASE_PORT"]
     database_keys = {key: os.getenv(key) for key in database}
     if not all(database_keys.values()):
-        raise ValueError("One or more database configuration variables are missing in the environment variables.")
-
+        logger.critical("One or more database configuration variables are missing in the " \
+        "environment variables.")
+        return None # type: ignore
+    
+    logger.info("Successfully retrieved all required secrets from environment variables.")
     return {
         "required_apis": required_apis,
         "database": database_keys
