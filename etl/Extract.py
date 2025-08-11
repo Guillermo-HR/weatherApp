22f4775ed9ff6dccfdad9b51332164ea90144378
Guillermo <guillermo.hre@gmail.com>
2025-08-11 14:45:04 -0600

    Refactor Extract class to accept logger as a parameter and improve URL construction in get_data method

--- etl/Extract.py
+++ etl/Extract.py
@@ -1,19 +1,18 @@
 import requests
-from datetime import datetime, timezone
 import logging
 from typing import Dict
 
 class Extract:
-    def __init__(self, api_name:str, api_key:str, constant_params:str, 
+    def __init__(self, logger: logging.Logger, api_name:str, api_key:str, constant_params:str, 
                  search_params:str, api_base_url:str,):
         self.api_name = api_name
         self.api_key = api_key
         self.api_constant_params = constant_params
         self.api_search_params = search_params
         self.api_base_url = api_base_url
-        self.logger = logging.getLogger(f"{api_name}_extractor")
+        self.logger = logger
 
-    def validate_coordinates(self, latitude:float, longitude:float) -> bool:
+    def validate_coordinates(self, latitude: float, longitude: float) -> bool:
         if not isinstance(latitude, (float, int)) or not isinstance(longitude, (float, int)):
             self.logger.error("Invalid coordinate type: latitude=%f, longitude=%f", 
                               latitude, longitude)
@@ -26,11 +25,12 @@ class Extract:
 
         return True
 
-    def get_data(self, latitude:float, longitude:float)->Dict:
+    def get_data(self, latitude: float, longitude: float)->Dict:
         if not self.validate_coordinates(latitude, longitude):
             return {"status": "failed"}
         
-        url = self.api_base_url + self.api_search_params.format(latitude=latitude, longitude=longitude)
+        url = self.api_base_url 
+        url += self.api_search_params.format(latitude = latitude, longitude = longitude)
         url += self.api_constant_params + self.api_key
         try:
             response = requests.get(url, timeout=10)
@@ -38,12 +38,10 @@ class Extract:
             data = response.json()
             return {"status": "success", "data": data}
         except requests.exceptions.RequestException as e:
-            timestamp = datetime.now(timezone.utc).timestamp()
             self.logger.error(
-                "API request failed for (%s) using (%s) at (%f): %s",
+                "API request failed for (%s) using (%s): %s",
                 self.api_name,
                 f"****{self.api_key[-4:]}",
-                timestamp,
                 f"{e}".replace(self.api_key, f"****{self.api_key[-4:]}")
             )
             return {"status": "failed"}
